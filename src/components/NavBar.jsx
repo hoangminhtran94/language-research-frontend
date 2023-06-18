f6fd6bbf2a0b38ea4b5146c76ca0a1b3f029fa71
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-18 11:56:46 -0400

    update navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,6 +11,9 @@ const Navbar = () => {
         <NavLink className="hover:scale-105 transition-all" to={"/"}>
           Home page
         </NavLink>
+        <h2 className="text-center flex-1">
+          Developed by Minh Hoang Tran - 041016957
+        </h2>
       </nav>
     </header>
   );
